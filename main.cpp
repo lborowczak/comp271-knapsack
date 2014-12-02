d839bedcaf88822e32363f3959defec4f56c7587
Lukasz Borowczak <lukasz.m.borowczak@gmail.com>
2014-12-02 16:33:33 -0600

    Added some more empty classes

--- main.cpp
+++ main.cpp
@@ -14,30 +14,45 @@ struct storableItem {
     };
 
 vector<storableItem> getItems();
-
+//vector<storableItem> calculateBestItemVal(vector<storableItem> listOfItems, int maxWeight, int maxVol);
+//void printItems(vector<storableItem> items);
 int main()
 {
     int maxWeight, maxVol; //variable to store the maximum weight the backpack can hold and the maximum volume it can hold
-    vector<storableItem> listOfItems; //initial vector of all items
-    cout << "How many items do you want to take with you?" << endl;  //ask user how many items they will want to take
-    cin >> numOfItems;
+    vector<storableItem> listOfItems, optimItems; //initial vector of all items and optimized
     cout << "What is the maximum weight that your backback can hold (in pounds)?" << endl; //ask user how much weight the backpack can hold
     cin >> maxWeight;
     cout << "What is the volume of the backpack (in cubic inches)?" << endl; //ask user what the volume of the backpack is
     cin >> maxVol;
 
     listOfItems = getItems();  //get the list of items from the user
+    //optimItems = calculateBestItemsVal(listOfItems, maxWeight, maxVol);
+    //printItems(optimItems);
 
     cout << "test" << endl;
     return 0;
 }
 
-vector<storableItem> getItems(int numOfItems)
+vector<storableItem> getItems()
 {
     vector<storableItem> items;
+    /*
+    item entry loop goes here
+    */
     return items;
 }
 
 /*
+vector<storableItem> calculateBestItemVal(vector<storableItem> listOfItems, int maxWeight, int maxVol)
+{
+
+
+
+}
+
+void printItems(vector<storableItem> items)
+{
+
+}
 
 */

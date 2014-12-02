24aea11e15c331f8fddf9fe1e0d2305d974169a2
Lukasz Borowczak <lukasz.m.borowczak@gmail.com>
2014-12-02 14:07:57 -0600

    Added more code, added and fixed some comments

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 //global so that we can create vectors of storableItem s
-//
+//Structure that stores the information needed for each item
 struct storableItem {
-    int area;
+    int volume;
     int weight;
     double price;
     char *itemName;
@@ -17,10 +17,15 @@ vector<storableItem> getItems(int numOfItems);
 
 int main()
 {
-    int numOfItems; //variable to store the number of items the person needs to bring, the maximum weight the backpack can hold, and the maximum
+    int numOfItems, maxWeight, maxVol; //variable to store the number of items the person needs to bring, the maximum weight the backpack can hold, and the maximum volume it can hold
     vector<storableItem> listOfItems; //initial vector of all items
-    cout << "How many items do you want to take with you?" << endl;
+    cout << "How many items do you want to take with you?" << endl;  //ask user how many items they will want to take
     cin >> numOfItems;
+    cout << "What is the maximum weight that your backback can hold (in pounds)?" << endl; //ask user how much weight the backpack can hold
+    cin >> maxWeight;
+    cout << "What is the volume of the backpack (in cubic inches)?" << endl; //ask user what the volume of the backpack is
+    cin >> maxVol;
+
     listOfItems = getItems(numOfItems);  //get the list of items
 
     cout << "test" << endl;

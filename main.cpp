a086d78816801eba495a79e15d587ca465c26edc
Lukasz Borowczak <lukasz.m.borowczak@gmail.com>
2014-12-03 15:29:16 -0600

    finished getItems() method, changed integers into doubles (to allow fractional weights and volumes), added comments

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 //global so that we can create vectors of storableItem s
 //Structure that stores the information needed for each item
+//doubles used so that we can accomodate fractions of a pound
 struct storableItem {
-    int volume;
-    int weight;
+    double volume;
+    double weight;
     double price;
-    char *itemName;
+    string itemName;
     };
 
 vector<storableItem> getItems();
@@ -18,10 +19,12 @@ vector<storableItem> getItems();
 //void printItems(vector<storableItem> items);
 int main()
 {
-    int maxWeight, maxVol; //variable to store the maximum weight the backpack can hold and the maximum volume it can hold
-    vector<storableItem> listOfItems, optimItems; //initial vector of all items and optimized
+    double maxWeight, maxVol; //variable to store the maximum weight the backpack can hold and the maximum volume it can hold
+    vector<storableItem> listOfItems, optimItems; //initial vector of all items and vector of the optimized items
+
     cout << "What is the maximum weight that your backback can hold (in pounds)?" << endl; //ask user how much weight the backpack can hold
     cin >> maxWeight;
+
     cout << "What is the volume of the backpack (in cubic inches)?" << endl; //ask user what the volume of the backpack is
     cin >> maxVol;
 
@@ -36,9 +39,33 @@ int main()
 vector<storableItem> getItems()
 {
     vector<storableItem> items;
-    /*
-    item entry loop goes here
-    */
+    string tempResponse = "", tempName;
+    storableItem *tempItem;
+    cout << "Please enter the information about your items:" << endl;
+
+    while (tolower(tempResponse[0]) != 'y') //Keep asking for items until they respond that they are done adding items
+    {
+        getline(cin, tempResponse); //fix input from previous use of cin so getline works
+        tempItem = new storableItem;
+
+        cout << "What is the name of the item?" << endl;
+        getline(cin, tempName); //allows use of spaces in input
+        tempItem->itemName = tempName;
+
+        cout << "How much does the item weigh (in pounds)?" << endl;
+        cin >> tempItem->weight;
+
+        cout << "How much space does the item take up (in cubic inches)?" << endl;
+        cin >> tempItem->volume;
+
+        cout << "what is the price of the item (in dollars)?" << endl;
+        cin >> tempItem->price;
+
+        items.push_back(*tempItem);
+
+        cout << "Are you done adding items (y/n)?" << endl;
+        cin >> tempResponse;
+    }
     return items;
 }
 
